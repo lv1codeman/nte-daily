3ff763ccb5d94832d2475c03427302f46695cccf
lv1codeman <lv1codeman@gmail.com>
2026-05-19 16:47:42 +0800

    發佈測試

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
+  compatibilityDate: "2026-05-19",
+  pages: true,
   css: ["vuetify/styles"],
   modules: ["vuetify-nuxt-module"],
+  ssr: false,
+
+  app: {
+    baseURL: import.meta.env.PROD ? "/nte-daily/" : "/",
+  },
 });
