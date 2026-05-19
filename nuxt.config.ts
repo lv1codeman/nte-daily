51a6d41f0c5a6c0446f484db3fb9ee3bf77ed273
lv1codeman <lv1codeman@gmail.com>
2026-05-19 17:15:30 +0800

    add devtools: { enabled: true },

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2026-05-19",
+  devtools: { enabled: true },
   pages: true,
   css: ["vuetify/styles"],
   modules: ["vuetify-nuxt-module"],
